24da0ff747ffd155ecb507e2409f6b4f580a1968
vixx_ <vittorio.adesso@gmail.com>
2021-07-04 17:42:16 -0400

    test: pip integration

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ CURRENT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]:-$0}))
 DIR=$(dirname $CURRENT_DIR)
 
 # pytest
-find $DIR/src -type f -not -path "*/__pycache__/*" -not -path "*/.pytest_cache/*"  -exec pytest {} +
+find $DIR/src/tests -type f -not -path "*/__pycache__/*" -not -path "*/.pytest_cache/*"  -exec pytest {} +
