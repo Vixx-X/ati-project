fb23cf52187e049cea008d8cf228dc528e02f05c
vixx_ <vittorio.adesso@gmail.com>
2021-07-08 06:16:35 -0400

    fix: jinja auto reload

--- src/config/dev.py
+++ src/config/dev.py
@@ -37,10 +37,16 @@ STATIC_FOLDER = f"{BASE_DIR}/static/"
 # Template folder
 TEMPLATE_FOLDER = f"{BASE_DIR}/templates/"
 
+# Languages
 LANGUAGES = {
     'en': 'English',
     'es': 'Español',
     'pt': 'português',
 }
 BABEL_TRANSLATION_DIRECTORIES = f"{BASE_DIR}/translations/"
+
+# Template Auto Reload
+TEMPLATES_AUTO_RELOAD = True
+
+
 
