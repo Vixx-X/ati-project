10c7d532005b5269fa01fd87c8a9785d9b711b88
Daniel <danielviei@hotmail.com>
2021-07-15 23:56:34 -0400

    feat: add javascript file

--- src/frontend/static_src/js/components/mobile-navmenu.ts
+++ src/frontend/static_src/js/components/mobile-navmenu.ts
@@ -1,9 +1,16 @@
-// function showDetails() {
-//     document.getElementById("details").classList.add('active-tab');
-//     document.getElementById("publication").classList.remove('active-tab');    
-// }
+function showDetails() {
+    document.getElementById("details").setAttribute("class", "active-content");
+    document.getElementById("tab-details").setAttribute("class", "active-tab");
+    document.getElementById("publication").setAttribute("class", "");
+    document.getElementById("tab-publication").setAttribute("class", "");
+}
 
-// function showPublication() {
-//     document.getElementById("publication").classList.add('active-tab');
-//     document.getElementById("details").classList.remove('active-tab');    
-// }
+function showPublication() {
+    document.getElementById("publication").setAttribute("class", "active-content");
+    document.getElementById("tab-publication").setAttribute("class", "active-tab");
+    document.getElementById("details").setAttribute("class", "");
+    document.getElementById("tab-details").setAttribute("class", "");
+}
+
+document.getElementById("buttonDetails").addEventListener("click", showDetails);
+document.getElementById("buttonPublication").addEventListener("click", showPublication);
