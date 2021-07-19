710cb4646a3e673870e76fe37e3913b77c345ae6
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-07-19 17:08:56 -0400

    create profile 2 view

--- src/backend/apps/user/views.py
+++ src/backend/apps/user/views.py
@@ -31,7 +31,9 @@ class Friend(BaseView):
     template_name = "user/friend-list.html"
 
     def get_context_data(self, pk):
-        return super().get_context_data()
+        ctx = super().get_context_data()
+        ctx["is_myuser"] = pk==1
+        return ctx
 
 
 class Profile(BaseView):
