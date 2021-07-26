1283faea53c292bb57256eafb1defce501e3bc41
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-07-26 04:07:03 -0400

    feat: create and done view publication page

--- src/backend/apps/posts/urls.py
+++ src/backend/apps/posts/urls.py
@@ -5,5 +5,5 @@ Urls for posts module/blueprint
 # from flask_babel import gettext as _
 from . import bp, views
 
-bp.add_url_rule("/post/<int:id>/", view_func=views.Post.as_view("post"))
+bp.add_url_rule("/<int:id>/", view_func=views.Post.as_view("post"))
 bp.add_url_rule("/comment/<int:id>/", view_func=views.Comment.as_view("comment"))
