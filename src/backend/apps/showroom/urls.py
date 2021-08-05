dc87155fd0f40dede2ea97c67ad04f6e7bade3a4
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-08-05 01:20:56 -0400

    feat: added some files to showrooms and done view of showrooms with names

--- src/backend/apps/showroom/urls.py
+++ src/backend/apps/showroom/urls.py
@@ -22,24 +22,29 @@ bp.add_url_rule("/", view_func=views.Index.as_view("index"))
 ##############
 
 list_of_components = [
+    ("boolean-button", ["Boolean Button", DV]),
     ("buttons", ["Buttons", GU]),
+    ("card", ["Card", DV]),
+    ("chat", ["Chat", GU]),
+    ("comments-users", ["Comments of Users", VA]),
+    ("drop-down-list", ["Drop Down List", ED]),
+    # ("element-friend-list", ["User Element of list", ED]),
+    ("footer-mobile", ["Footer Mobile", DV]),
+    ("footer", ["Footer", ED]),
+    ("header_publication",  ["Header Publication", DV]),
     ("header", ["Header", GU]),
+    ("header2", ["Home Header", GU]),
+    ("heading", ["Heading", DV]),
+    ("input-label", ["Input Label", DV]),
+    ("list-chats", ["List Chats", VA]),
+    ("list-comments", ["List Comments", ED]),
+    ("list-friends", ["List Friends", ED]),
+    ("long-cards", ["Long Card", DV]),
+    ("mobile-navmenu", ["Nav Menu Profile", VA]),
+    ("notification", ["Notification", VA]),
+    ("radio-button", ["Radio Button", DV]),
+    ("search-button", ["Search Button", ED]),
     ("user_icon", ["User Icons", VA]),
-    ("card", "Card - Daniel Vieira"),
-    ("header2", "Main Header - Gabriela Ustariz"),
-    ("comments-users", "Comments of Users - Vittorio Adesso"),
-    ("footer", "Footer - Eduardo Saurez"),
-    ("heading", "Heading - Daniel Vieira"),
-    ("long-cards", "Long Cards - Daniel Vieira"),
-    ("notification", "Notification - Vittorio Adesso"),
-    ("search-button", "Search Button - Eduardo Suarez"),
-    ("chat", "Chat - Eduardo Suarez"),
-    ("header_publication", "Header Publication - DanielVieria"),
-    ("input-label", "Input Label - Daniel Vieria"),
-    ("list-friends", "list-friends - Eduardo Suarez"),
-    ("boolean-button", "Boolean Button - Daniel Vieria"),
-    ("drop-down-list", "Drop Down List - Eduardo Suarez"),
-    ("footer-mobile", "Footer Mobile - Daniel Vieria"),
 ]
 
 bp.add_url_rule("/components/", view_func=views.Components.as_view("components"))
@@ -64,34 +69,13 @@ bp.add_url_rule(
 )
 bp.add_url_rule("/input-label/", view_func=views.InputLabel.as_view("input-label"))
 bp.add_url_rule("/radio-button/", view_func=views.RadioButton.as_view("radio-button"))
-
-
-#########
-# Views #
-#########
-
-list_of_views = [
-    ("muro", "Muro"),
-    ("home", "Home"),
-    ("login", "LogIn"),
-    ("register", "Register"),
-    ("configurations", "Configurations"),
-    ("notifications", "Notifications"),
-    ("profile1", "Profile"),
-    ("boolean-button", "Boolean Button - Daniel Vieria"),
-    ("drop-down-list", "Drop Down List - Eduardo Suarez"),
-    ("footer-mobile", "Footer Mobile - Daniel Vieria"),
-    ("list-chats", "List Chats - Daniel Vieria"),
-    ("element-friend-list", "Element Friend List - Eduardo Suarez"),
-    ("CreatePublications", "CreatePublications - Eduardo Suarez"),
-    ("mobile-navmenu", "Mobile Nav Menu - Gabriela Uztariz"),
-    ("list-comments", "List Comments - Eduardo Suarez"),
-]
-
-bp.add_url_rule("/views/", view_func=views.Views.as_view("views"))
-bp.add_url_rule("/muro/", view_func=views.Muro.as_view("muro"))
-bp.add_url_rule("/home/", view_func=views.Home.as_view("home"))
-bp.add_url_rule("/login/", view_func=views.LogIn.as_view("login"))
+bp.add_url_rule(
+    "/mobile-navmenu/", view_func=views.MobileNavMenu.as_view("mobile-navmenu")
+)
+bp.add_url_rule(
+    "/list-comments/", view_func=views.ListComments.as_view("list-comments")
+)
+bp.add_url_rule("/list-chats/", view_func=views.ListChats.as_view("list-chats"))
 bp.add_url_rule(
     "/boolean-button/", view_func=views.BooleanButton.as_view("boolean-button")
 )
@@ -105,22 +89,49 @@ bp.add_url_rule(
 bp.add_url_rule(
     "/footer-mobile/", view_func=views.FooterMobile.as_view("footer-mobile")
 )
+
+
+#########
+# Views #
+#########
+
+list_of_views = [
+    ("chatView", ["Chat", GU]),
+    ("configurations", ["Configuratios", ED]),
+    ("createpublication", ["Create Publication", ED]),
+    ("forgotpassword", ["Forgot Password", VA]),
+    ("friendlist", ["Friend List", ED]),
+    ("home", ["Home", GU]),
+    ("landingpage", ["Landing", VA]),
+    # ("login", ["Login", VA]),
+    ("modifypublication", ["Modify Publication", ED]),
+    ("notifications", ["Notifications", DV]),
+    ("post", ["Post", GU]),
+    ("profile", ["Profile", GU]),
+    ("profiledescription", ["Profile Description", GU]),
+    ("profileedit", ["Profile Edit", DV]),
+    ("register", ["Register", VA]),
+    ("searchpage", ["Search Page", DV]),
+]
+
+bp.add_url_rule("/views/", view_func=views.Views.as_view("views"))
+bp.add_url_rule("/chatView/", view_func=views.ChatView.as_view("chatView"))
+bp.add_url_rule("/home/", view_func=views.Home.as_view("home"))
+bp.add_url_rule("/login/", view_func=views.LogIn.as_view("login"))
 bp.add_url_rule("/register/", view_func=views.Register.as_view("register"))
-bp.add_url_rule("/list-chats/", view_func=views.ListChats.as_view("list-chats"))
 bp.add_url_rule(
     "/notifications/", view_func=views.Notifications.as_view("notifications")
 )
 bp.add_url_rule(
     "/configurations/", view_func=views.Configurations.as_view("configurations")
 )
-bp.add_url_rule("/profile1/", view_func=views.Profile1.as_view("profile1"))
-bp.add_url_rule(
-    "/CreatePublications/",
-    view_func=views.CreatePublications.as_view("CreatePublications"),
-)
-bp.add_url_rule(
-    "/mobile-navmenu/", view_func=views.MobileNavMenu.as_view("mobile-navmenu")
-)
-bp.add_url_rule(
-    "/list-comments/", view_func=views.ListComments.as_view("list-comments")
-)
+bp.add_url_rule("/profile/", view_func=views.Profile.as_view("profile"))
+bp.add_url_rule("/createpublication/", view_func=views.CreatePublication.as_view("createpublication"))
+bp.add_url_rule("/forgotpassword/", view_func=views.ForgotPassword.as_view("forgotpassword"))
+bp.add_url_rule("/friendlist/", view_func=views.FriendList.as_view("friendlist"))
+bp.add_url_rule("/landingpage/", view_func=views.LandingPage.as_view("landingpage"))
+bp.add_url_rule("/modifypublication/", view_func=views.ModifyPublication.as_view("modifypublication"))
+bp.add_url_rule("/post/", view_func=views.Post.as_view("post"))
+bp.add_url_rule("/profiledescription/", view_func=views.ProfileDescription.as_view("profiledescription"))
+bp.add_url_rule("/profileedit/", view_func=views.ProfileEdit.as_view("profileedit"))
+bp.add_url_rule("/searchpage/", view_func=views.SearchPage.as_view("searchpage"))
