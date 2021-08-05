632c9b8b3f0a40ab24172149c8d01d90f64bbc77
EduhuSua <eduardosuarez.ucv@gmail.com>
2021-08-05 16:46:31 -0400

    Finishing the preview when a image is charge

--- src/frontend/static_src/js/components/create-publication.ts
+++ src/frontend/static_src/js/components/create-publication.ts
@@ -2,17 +2,18 @@ console.log("Hola mundo nenes desde create publication");
 
 const handleFiles = function(event){
     let reader = new FileReader();
-    reader.onload = function(){
+    let file = event.target.files[0]
+
+    reader.onloadend  = function(){
         let preview = document.getElementById('preview');
         let new_Div = document.createElement("div")
-        new_Div.setAttribute("class","create-publication-body-input-label")
+            new_Div.setAttribute("class","create-publication-body-input-label")
         let new_img = document.createElement("img")
-        console.log(reader.result[0])
-        new_img.src = reader.result[0]
-        new_img.setAttribute("style","width:150px;");
+        new_img.src = reader.result
         new_Div.appendChild(new_img)
         preview.appendChild(new_Div)
     };
-    reader.readAsDataURL(event.target.files[0])
+
+    reader.readAsDataURL(file)
 }
 let input_file = document.getElementById("file").addEventListener("change",handleFiles)
