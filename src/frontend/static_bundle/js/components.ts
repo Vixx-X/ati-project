ed594f42457aa6d7ac60e851efd64585ed39ecce
EduhuSua <eduardosuarez.ucv@gmail.com>
2021-08-05 22:48:26 -0400

    finishing the functionality of the search bar

--- src/frontend/static_bundle/js/components.ts
+++ src/frontend/static_bundle/js/components.ts
@@ -1,4 +1,5 @@
+import "js/components/search.ts";
 import "js/components/chat.ts";
-import "js/components/changeTheme.ts";
+ import "js/components/changeTheme.ts";
 import "js/components/description-input.ts";
 import "js/components/editCard.ts";
