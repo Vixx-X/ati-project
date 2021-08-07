24c43fceffd1ae3c9ee465fbcfd4bf3cf0d5d65d
EduhuSua <eduardosuarez.ucv@gmail.com>
2021-08-07 13:05:43 -0400

    Finishing the functionality of the reply button

--- src/frontend/static_bundle/js/components.ts
+++ src/frontend/static_bundle/js/components.ts
@@ -1,6 +1,7 @@
+import "js/components/reply-button.ts";
 import "js/components/search.ts";
 import "js/components/chat.ts";
- import "js/components/changeTheme.ts";
+import "js/components/changeTheme.ts";
 import "js/components/create-publication.ts";
 import "js/components/mobile-navmenu.ts";
 import "js/components/changeTheme.ts";
