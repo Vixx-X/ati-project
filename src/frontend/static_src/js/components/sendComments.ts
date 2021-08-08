911f1f0875453cd252fa55734cc1275d5dfab564
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-08-07 20:25:20 -0400

    feat: delete console.log messages

--- src/frontend/static_src/js/components/sendComments.ts
+++ src/frontend/static_src/js/components/sendComments.ts
@@ -8,11 +8,8 @@ function sendComments(){
     if (content === ""){alert('El mensaje está vacio'); return;}
     const template = document.querySelector('.item.left');
     const message = template.cloneNode(true);
-    console.log(message);
     const nodeContent = message.querySelector('.cont-chat p');
-    console.log(nodeContent.textContent);
     nodeContent.textContent = content;
-    console.log(nodeContent.textContent);
     const time = message.querySelector('.cont-chat-description p');
     var d = new Date();
     time.textContent = `${d.getHours()} : ${d.getMinutes()}`;
