ecdddc8d1a2fbada9c09fcd96bd6a712dde634ca
Daniel <danielviei@hotmail.com>
2021-09-23 23:24:36 -0400

    hotfix: fix for windows path

--- src/wsgi.py
+++ src/wsgi.py
@@ -5,15 +5,15 @@ Starting script for the flask app
 
 import os
 
+# from flask_restful import Api
 from dotenv import load_dotenv
 import click
 
 from backend import init_app
+# from backend.apps.api.urls import initializeRoutes
 
-from loaddata import loaddata as ld
-
-ENV_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + "/env/"
-load_dotenv(ENV_DIR + ".env")
+ENV_DIR = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "env")
+load_dotenv(os.path.join(ENV_DIR, ".env"))
 
 app = init_app()
 
@@ -28,8 +28,11 @@ def loaddata(filename, dry_run=False):
 
     DRY_RUN (bool) load data without saving it into the db.
     """
+    from loaddata import loaddata as ld
     return ld(filename, dry_run)
 
+# api = Api(app)
+# initializeRoutes(api)
 
 if __name__ == "__main__":
     from sys import argv
