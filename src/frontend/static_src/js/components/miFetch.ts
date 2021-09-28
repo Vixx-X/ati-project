3c998574b3caa4982906b55e013af5628945d514
vixx_ <vittorio.adesso@gmail.com>
2021-09-28 00:58:28 -0400

    feat: macro with context

--- src/frontend/static_src/js/components/miFetch.ts
+++ src/frontend/static_src/js/components/miFetch.ts
@@ -1,8 +1,8 @@
-function miFetch (...args) {
-    return(fetch(...args))
+function miFetch(...args) {
+  return fetch(...args);
 }
-function handleClick(){
-    console.log(miFetch("localhost:9021/api/post"));
+function handleClick() {
+  console.log(miFetch("localhost:9021/api/post"));
 }
 
-document.getElementById("boton_edu").addEventListener("click",handleClick)
+document.getElementById("boton_edu")?.addEventListener("click", handleClick);
